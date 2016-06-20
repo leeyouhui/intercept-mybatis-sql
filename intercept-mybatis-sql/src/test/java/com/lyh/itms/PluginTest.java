2a31dc0fb4b8a4dc12b8b3c4ef7ad633f3f2cfca
G <G@liyouhui-PC>
2016-06-20 19:08:04 +0800

    lyh

---
+++ intercept-mybatis-sql/src/test/java/com/lyh/itms/PluginTest.java
@@ -0,0 +1,40 @@
+package com.lyh.itms;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.junit.Test;
+
+public class PluginTest {
+
+	@Test
+	public void test() {
+		Map map = new HashMap();  
+	    map = (Map) new MyMapPlugin().plugin(map);
+	    System.out.println(map.get("Anything"));
+	}
+
+}
+
+@Intercepts({@Signature(type = Map.class, method = "get", args = {Object.class})})
+class MyMapPlugin implements Interceptor{
+
+	public Object intercept(Invocation invocation) throws Throwable {
+//		return invocation.proceed();
+		return "trs";
+	}
+
+	public Object plugin(Object target) {
+		return Plugin.wrap(target, this);
+	}
+
+	public void setProperties(Properties properties) {
+	}
+	
+}
